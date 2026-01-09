a28afa22b36626be66fbad1365641a64d69136df
RomeoFroger49 <romeo.froger@gmail.com>
2026-01-09 12:34:00 +0100

    test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[])
 {
     Column c1 = Column("c1");
     Column c2 = Column("c2");
+    Column c3 = Column("c3");
+    Column c4 = Column("c4");
 
     c1.insertValue(56);
     c1.insertValue(6);
@@ -21,11 +23,27 @@ int main(int argc, char const *argv[])
     c2.insertValue(43);
     c2.insertValue(4);
 
-    CDataframe cd = CDataframe({c1, c2});
+    c3.insertValue(19);
 
-    std::vector<Column> subset = {c1};
+    c4.insertValue(324);
+    c4.insertValue(1);
+    c4.insertValue(6);
+    c4.insertValue(6);
+    c4.insertValue(6);
 
-    cd.byCol(subset);
+    CDataframe cd = CDataframe({c1, c2, c3, c4});
+
+    cd.rmCol(&c2);
+
+    cd.addCol(&c2);
+
+    cd.renameCol(&c2, "test");
+
+    cd.display();
+    std::cout << "--------\n";
+
+    int val = 6;
+    std::cout << "does " << val << " exist ? " << (cd.exist(val) ? "true" : "false") << std::endl;
 
     return 0;
 }
